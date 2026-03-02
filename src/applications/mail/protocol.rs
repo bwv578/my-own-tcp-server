b69af8461f04370cc82037c02627f639277749bc
rustserver <rustserver@ip-172-31-35-8.ap-northeast-2.compute.internal>
2026-03-02 09:44:13 +0000

    VPC - EHLO test completed after adding MX record

--- src/applications/mail/protocol.rs
+++ src/applications/mail/protocol.rs
@@ -1,15 +1,19 @@
 use std::error::Error;
-use std::io::{BufRead, BufReader, Read};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{SocketAddr, TcpStream};
 use std::sync::Arc;
 use rustls::ServerConfig;
 use crate::applications::model::Protocol;
 
 pub struct Smtp {
+    domain: String
 }
 
 impl Protocol for Smtp {
-    fn handle_connection(&self, stream: TcpStream, peer: SocketAddr, config: Option<Arc<ServerConfig>>) -> Result<(), Box<dyn Error>> {
+    fn handle_connection(&self, mut stream:TcpStream, peer: SocketAddr, config: Option<Arc<ServerConfig>>) -> Result<(), Box<dyn Error>> {
+        let msg_ready = format!("220 {} ESMTP ready\r\n", &self.domain);
+        stream.write_all(msg_ready.as_bytes())?;
+
         let mut line_buf = String::new();
         let mut reader = BufReader::new(&stream);
         reader.read_line(&mut line_buf)?;
@@ -27,7 +31,7 @@ impl Protocol for Smtp {
 }
 
 impl Smtp {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(domain:&str) -> Self {
+        Self {domain: domain.to_string()}
     }
 }
