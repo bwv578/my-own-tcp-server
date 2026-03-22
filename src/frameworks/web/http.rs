7c26d745cd8797c970d8cccd22685b13c1bab7d3
smh <bwv578g@gmail.com>
2026-03-22 23:44:37 +0900

    fix: frameworks::web::extract - 참조카운트>1, try_clone 불가. RwLock 대신 Mutex<Option<>>로 감싸고 extract시 take로 소유권 추출

--- src/frameworks/web/http.rs
+++ src/frameworks/web/http.rs
@@ -1,26 +1,25 @@
 use std::io;
-use std::sync::{Arc, OnceLock, RwLock, RwLockWriteGuard};
+use std::sync::{Mutex, MutexGuard, OnceLock};
 use crate::applications::async_web::http::{HttpRequest, HttpResponse, Method};
 use crate::applications::async_web::protocol::Http;
 
-static HTTP_MVC:OnceLock<Arc<RwLock<Http>>> = OnceLock::new();
+static HTTP_MVC:OnceLock<Mutex<Option<Http>>> = OnceLock::new();
 
-fn as_ref_mut() -> RwLockWriteGuard<'static, Http> {
+fn as_guard() -> MutexGuard<'static, Option<Http>> {
     HTTP_MVC.get_or_init(|| {
-            Arc::new(RwLock::new(Http::new()))
+            Mutex::new(Some(Http::new()))
         })
-        .write()
-        .unwrap()
+        .lock().unwrap()
 }
 
 pub fn route<Fut, F>(method:Method, path:&str, action: F)
 where
     Fut: Future<Output = io::Result<usize>> + Send + 'static,
     F: Fn(HttpRequest, HttpResponse) -> Fut + Send + Sync + 'static,
-{ as_ref_mut().handle(method, path, action); }
+{ as_guard().as_mut().unwrap().handle(method, path, action); }
 
 pub fn extract() -> Http {
-    let clone = Arc::clone( HTTP_MVC.get().unwrap() );
-    let rwlock = Arc::try_unwrap(clone).ok().unwrap();
-    rwlock.into_inner().unwrap()
+    HTTP_MVC.get().unwrap()
+        .lock().unwrap()
+        .take().unwrap()
 }
